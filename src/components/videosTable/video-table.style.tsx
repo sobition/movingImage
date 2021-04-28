79c1d5d3bd0e8a6ae21ffd64ed917e0552e356b9
S.Aminnejad <s.aminnejad2013@gmail.com>
2021-04-29 02:34:48 +0430

    removed search container from video table

--- src/components/videosTable/video-table.style.tsx
+++ src/components/videosTable/video-table.style.tsx
@@ -13,48 +13,7 @@ export const CustomisedTable = styled(Table)`
   }
 `;
 
-export const SearchForm = styled.form`
-  min-height: 50px;
-  margin-bottom: 24px;
-  input {
-    height: 50px;
-    border: 1px solid #ddd;
-    border-radius: 4px 0 0 4px;
-    box-sizing: border-box;
-    padding: 0 12px;
-    outline: none;
-    font-size: 14px;
-  }
 
-  Button {
-    height: 50px;
-    border-radius: 0 4px 4px 0;
-    margin: 0;
-    background-color: blue;
-    color: #fff;
-  }
-`;
-
-export const SearchedTag = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 12px 0px;
-  width: fit-content;
-  padding: 5px 10px;
-  background-color: #ddd;
-  border-radius: 12px;
-  color: #222;
-  div {
-    margin: 0 5px;
-  }
-  svg {
-    width: 12px;
-
-    :hover {
-      cursor: pointer;
-    }
-  }
-`;
 
 export const CustomTableCell = styled(TableCell)`
   Button {
--- src/components/videosTable/videos-table.tsx
+++ src/components/videosTable/videos-table.tsx
@@ -3,14 +3,13 @@ import { Paper, TableBody, TableCell, TableContainer, TableHead, TableRow, Butto
 import { ProcessedVideo } from '../../common/interfaces';
 import { useHistory } from 'react-router-dom/';
 import VideosContext from '../videos-context';
-import { CustomisedTable, CustomTableCell, SearchForm, SearchedTag } from './video-table.style';
-
+import { CustomisedTable, CustomTableCell } from './video-table.style';
+import FilterMenu from '../filtersMenu/filters-menu';
 const VideosTable: React.FC = () => {
   const videoContext = useContext(VideosContext);
   const [key, setKey] = useState(1);
   const [displayingResult, setDisplayingResult] = useState<ProcessedVideo[]>([]);
-  const [searchPhrase, setSearchPhrase] = useState('');
-  const [searchIsSet, setSearchIsSet] = useState<Boolean>(false);
+
   let history = useHistory();
   const deleteVideo = (index: number) => {
     let approval = window.confirm('This action will permenantely delete the selected video');
@@ -18,32 +17,6 @@ const VideosTable: React.FC = () => {
     if (approval) {
       videos.splice(index, 1);
       setKey(key === 1 ? 2 : 1);
-    }
-  };
-
-  const resetSearch = () => {
-    setSearchPhrase('');
-    setSearchIsSet(false);
-    setDisplayingResult(videoContext.videos);
-  };
-
-  const searchVideo = (phrase: string) => {
-    let result: ProcessedVideo[] = [];
-    if (phrase) {
-      videoContext.videos.map((video: ProcessedVideo) => {
-        let videoName: string = video.name.toUpperCase();
-        if (videoName.includes(phrase.toUpperCase())) {
-          result.push(video);
-        }
-      });
-      if (result.length > 0) {
-        setDisplayingResult(result);
-        setSearchIsSet(true);
-      } else {
-        alert('No result is found. Please try something else');
-      }
-    } else {
-      alert('please enter a phrase to search');
     }
   };
 
@@ -55,28 +28,7 @@ const VideosTable: React.FC = () => {
 
   return (
     <>
-      <SearchForm id={'searchForm'} onSubmit={() => searchVideo(searchPhrase)}>
-        <input
-          id={'searchField'}
-          value={searchPhrase}
-          onChange={(e) => setSearchPhrase(e.target.value)}
-          placeholder={"search video's name"}
-        />
-        <Button id="searchBtn" onClick={() => searchVideo(searchPhrase)}>
-          Search
-        </Button>
-        {searchIsSet ? (
-          <SearchedTag>
-            <div>{searchPhrase}</div>
-            <svg id={'resetSearch'} onClick={() => resetSearch()} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512.001">
-              <path d="M284.284 256L506.142 34.142c7.811-7.81 7.811-20.474 0-28.284-7.811-7.811-20.474-7.811-28.284 0L256 227.716 34.142 5.858c-7.811-7.811-20.474-7.811-28.284 0-7.811 7.81-7.811 20.474 0 28.284L227.716 256 5.858 477.858c-7.811 7.811-7.811 20.474 0 28.284 7.81 7.81 20.473 7.811 28.284 0L256 284.284l221.858 221.858c7.81 7.81 20.473 7.811 28.284 0s7.811-20.474 0-28.284L284.284 256z" />
-            </svg>
-          </SearchedTag>
-        ) : (
-          ''
-        )}
-      </SearchForm>
-
+      <FilterMenu updateResult={setDisplayingResult} />
       <TableContainer component={Paper} style={{ marginTop: '40px' }}>
         <CustomisedTable>
           <TableHead>
